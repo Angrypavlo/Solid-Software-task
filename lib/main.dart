9656b3bba96a2d33b46ab501964dc0475a7a4fa2
angrypavlo <angrypavlo@gmail.com>
2026-03-11 16:58:24 +0200

    first simplified version of the application

---
+++ lib/main.dart
@@ -0,0 +1,74 @@
+import 'dart:math';
+
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      theme: ThemeData(colorScheme: .fromSeed(seedColor: Colors.deepPurple)),
+      home: const MyHomePage(title: 'Flutter Demo Home Page'),
+    );
+  }
+}
+
+class MyHomePage extends StatefulWidget {
+  const MyHomePage({super.key, required this.title});
+
+  final String title;
+
+  @override
+  State<MyHomePage> createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  int _counter = 0;
+  int a = 100;
+  int b = 0;
+  int c = 0;
+  int d = 0;
+  void _incrementCounter() {
+    setState(() {
+      _counter++;
+      b = Random().nextInt(256);
+      c = Random().nextInt(256);
+      d = Random().nextInt(256);
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        backgroundColor: Theme.of(context).colorScheme.inversePrimary,
+        title: Text(widget.title),
+      ),
+      body: GestureDetector(
+        onTap: _incrementCounter,
+        child: Container(
+          width: double.infinity,
+          height: double.infinity,
+
+          color: Color.fromARGB(100, b, c, d),
+          child: Column(
+            mainAxisAlignment: .center,
+            children: [
+              const Text('Color have been changed this many times:'),
+              Text(
+                '$_counter',
+                style: Theme.of(context).textTheme.headlineMedium,
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
